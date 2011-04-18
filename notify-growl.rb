b809c7c58a8f5149eb41a0f9279be0e3dd149f69
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2011-04-18 23:58:23 +0900

    ふぁぼ通知もいれたよ

--- notify-growl.rb
+++ notify-growl.rb
@@ -5,9 +5,13 @@ Module.new do
   g = Growl.new '127.0.0.1',"ruby-growl", ["ruby-growl Notification"]
   # 取り敢えずメンションがきたら表示するだけ
   plugin.add_event(:mention) do |post, message|
-    message.each{|m| 
-      msg = m.to_s
-      g.notify "ruby-growl Notification", 'mikumiku:mention', msg
+    message.each{ |msg|
+      m = msg.to_s
+      g.notify "ruby-growl Notification", "mikumiku:Mention", msg.to_s
     }
+  end
+  #ふぁぼられると通知
+  plugin.add_event(:favorite) do |post, user, message|
+    g.notify "ruby-growl Notification", "mikumiku:Favorite by #{user[:idname]}", message.to_s
   end
 end
