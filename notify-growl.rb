5c7bb982d97abd0aef0a4f864de1a489f87d783c
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2011-04-23 12:09:10 +0900

    あんふぁぼを通知するよ

--- notify-growl.rb
+++ notify-growl.rb
@@ -4,29 +4,39 @@ Module.new do
   plugin = Plugin::create(:notify)
   g = Growl.new '127.0.0.1',"ruby-growl", ["ruby-growl Notification"]
   str = "mikumiku:"
+  notification = "ruby-growl Notification"
   # 取り敢えずメンションがきたら表示するだけ
   plugin.add_event(:mention) do |service, message|
-    message.each{ |msg|
+    message.each do |msg|
       user = msg.idname
-      g.notify "ruby-growl Notification", str+"Mention from #{user}", msg.to_show
-    }
+      g.notify notification, str+"Mention from #{user}", msg.to_show
+    end
   end
   #ふぁぼられると通知
   plugin.add_event(:favorite) do |post, user, message|
-    u = message.idname
-    g.notify "ruby-growl Notification", str+"Favorite by #{u}", message.to_show
+    f = post.idname 
+    u = user.idname 
+    m = message.to_show
+    g.notify notification, str+"Favorite by #{u}", m unless f == u
+  end
+  #あんふぁぼをされると通知
+  plugin.add_event(:unfavorite) do |post, user, message|
+    f = post.idname
+    u = user.idname
+    m = message.to_show
+    g.notify notification, str+"Unfavorite by #{u}", m
   end
   #タイムライン更新通知
   plugin.add_event(:update) do |service, message|
-    message.each{|msg|
+    message.each do |msg|
       user = msg.idname
-      g.notify "ruby-growl Notification", str+user, msg.to_show 
-    }
+      g.notify notification, str+user, msg.to_show 
+    end
   end
   #ふぉろー通知
   plugin.add_event(:followers_created) do |post, users|
     users.each do |user|
-      g.notify "ruby-growl Notification", str+user.idname, "ふぉろーされました"
+      g.notify notification, str+user.idname, "ふぉろーされました"
     end
   end
 
@@ -34,7 +44,7 @@ Module.new do
   # core/addon/notify.rbでは:followers_destroyを利用しているみたい…
   plugin.add_event(:followers_destroy) do |post, users|
     users.each do |user| 
-      g.notify "ruby-growl Notification", str+user.idname, "りむられました"
+      g.notify notification, str+user.idname, "りむられました"
     end
   end
 end
