693fb031d7d8536157ab12c99670e426605b05e2
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2011-04-25 02:30:58 +0900

    音通知もできるようになったよ

--- notify-growl.rb
+++ notify-growl.rb
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
 require 'ruby-growl'
+require 'sdl'
 Module.new do 
+  SDL.init(SDL::INIT_AUDIO)
+  SDL::Mixer.open
   plugin = Plugin::create(:notify)
+  mdir= DEFAULT_SOUND_DIRECTORY
   g = Growl.new '127.0.0.1',"ruby-growl", ["ruby-growl Notification"]
+  # g.register
   str = "mikumiku:"
   notification = "ruby-growl Notification"
+
+  def self.sound(file)
+    music = SDL::Mixer::Music.load(file)
+    SDL::Mixer.play_music music, 1
+  end
   # 取り敢えずメンションがきたら表示するだけ
   plugin.add_event(:mention) do |service, message|
     message.each do |msg|
       user = msg.idname
       g.notify notification, str+"Mention from #{user}", msg.to_show
+      sound(mdir + "/message-received.wav")
     end
   end
   #ふぁぼられると通知
@@ -18,6 +29,7 @@ Module.new do
     u = user.idname 
     m = message.to_show
     g.notify notification, str+"Favorite by #{u}", m unless f == u
+    sound(mdir + "/favo.wav")
   end
   #あんふぁぼをされると通知
   plugin.add_event(:unfavorite) do |post, user, message|
@@ -25,12 +37,14 @@ Module.new do
     u = user.idname
     m = message.to_show
     g.notify notification, str+"Unfavorite by #{u}", m
+    sound(mdir + "/favo2.wav")
   end
   #タイムライン更新通知
   plugin.add_event(:update) do |service, message|
     message.each do |msg|
       user = msg.idname
       g.notify notification, str+user, msg.to_show 
+      sound(mdir+"/mikutter.wav")
     end
   end
   #ふぉろー通知
