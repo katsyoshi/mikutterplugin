04dac509824b2976cc4184ace5f877e39575a66b
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2011-10-24 15:38:32 +0900

    added sound patch for mac

---
+++ zenra.rb
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+require 'pebbles/zenra'
+# Module.new do
+# end
+Pebbles::Zenra.app_id="Hwpy0dSxg67ebRxFKpqHvy_p2jojI3M12kduIL4csNrbxi02kZlJcPDeGnro5W9cgR.Mhg--"
+# znr="ちなみに私は相当手が大きいのでおっぱい揉みたい"
+znr="この全裸プラグインはYahooAPIが必要なのでやっぱりやめにするか"
+p Pebbles::Zenra.new.zenrize znr
